3f8067f740b39e3319c2d1533a6a08390c23f01f
Wilson Su <wilson_su@trend.com.tw>
2018-08-27 15:45:52 +0800

    add configure script for setting up options for connection

---
+++ configure.js
@@ -0,0 +1,53 @@
+const prompt = require('prompt');
+const util = require('./src/common/util');
+
+const generate = options => `
+  const ${options.bot} = require('./src/hearts/${options.bot}');
+  module.exports = {
+    server: '${options.server}',
+    token: '${options.token}',
+    playerNumber: ${options.number},
+    playerName: '${options.name}',
+    bot: new ${options.bot}(),
+    logs: path.join(__dirname, '${options.logs}'),
+  };
+`;
+
+prompt.start();
+
+prompt.get({
+  properties: {
+    server: {
+      description: 'Enter server address',
+      default: 'ws://localhost:8080',
+      required: true
+    },
+    token: {
+      description: 'Enter player token',
+      message: 'Token is required',
+      required: true
+    },
+    number: {
+      description: 'Enter player number',
+      type: 'number',
+      message: 'Player number is required',
+      required: true
+    },
+    name: {
+      description: 'Enter player name',
+      message: 'Player name is required',
+      required: true
+    },
+    bot: {
+      description: 'Enter bot module name',
+      message: 'Bot module name is required',
+      required: true
+    },
+    logs: {
+      description: '(optional) Enter logs detination folder',
+      default: 'logs',
+    },
+  }
+}, (error, result) => {
+  util.file.write('config.js', generate(result), { type: 'text' });
+});
