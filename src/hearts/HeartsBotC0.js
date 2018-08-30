feb35c0b31c10d680aad456e9daf9d6a5a839ce1
Wilson Su <wilson_su@trend.com.tw>
2018-08-30 15:55:37 +0800

    update BotC0

--- src/hearts/HeartsBotC0.js
+++ src/hearts/HeartsBotC0.js
@@ -4,21 +4,41 @@ const { Cards, Card } = require('./HeartsDataModels');
 class HeartsBotC0 extends HeartsBotBase {
   pass (middleware) {
     const cards = middleware.hand.cards;
-    const highest = new Cards([
-      cards.spades.max,
-      cards.hearts.max,
-      cards.diamonds.max,
-      cards.clubs.max,
-    ]);
-    return highest.sort(false).list.slice(0, 3);
+    const high = [];
+    [].push(
+      ...cards.spades.sort(false).list.slice(0, 3),
+      ...cards.hearts.sort(false).list.slice(0, 3),
+      ...cards.diamonds.sort(false).list.slice(0, 3),
+      ...cards.clubs.sort(false).list.slice(0, 3),
+    );
+    return new Cards(high).sort(false).list.slice(0, 3);
   }
 
   expose (middleware) {
-    return [];
+    const cards = middleware.hand.cards;
+    const spades = cards.spades.ge('QS');
+    const hearts = cards.hearts.ge('TH');
+    return spades.length > 2 && hearts.length > 3 ? [] : ['AH'];
   }
 
   pick (middleware) {
-    return middleware.hand.valid.random();
+    const round = middleware.round;
+    const hand = middleware.hand;
+    const valid = middleware.hand.valid;
+    if (round.isFirst) {
+      return valid.min;
+    }
+    if (!hand.canFollowLead) {
+      return valid.find('QS') || valid.hearts.max || valid.max;
+    }
+    const followed = round.followed;
+    if (round.isLast && !round.hasPenaltyCard) {
+      return valid.max;
+    }
+    if (round.isLast && round.hasPenaltyCard) {
+      return valid.lt(followed.max).max || valid.max;
+    }
+    return valid.lt(followed.max).max || valid.gt(followed.max).min;
   }
 }
 
