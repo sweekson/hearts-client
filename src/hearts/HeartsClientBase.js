878313c2d6dfa5e7ac5af2f498bf1fe77b460a01
Wilson <sweekson@gmail.com>
2018-09-03 11:46:30 +0800

    add models for local client

---
+++ src/hearts/HeartsClientBase.js
@@ -0,0 +1,45 @@
+const EventEmitter = require('events');
+
+class HeartsClientBase extends EventEmitter {
+  constructor (options) {
+    super();
+    this.options = options;
+    this.middlewares = [];
+    this.initialize();
+  }
+
+  initialize () {
+    (this.options.middlewares || []).forEach(v => this.use(v));
+  }
+
+  pick (dealNumber, roundNumber, turnCard) {
+    this.send('pick_card', { dealNumber, roundNumber, turnCard });
+  }
+
+  expose (dealNumber, cards) {
+    this.send('expose_my_cards', { dealNumber, cards });
+  }
+
+  pass (dealNumber, cards) {
+    this.send('pass_my_cards', { dealNumber, cards });
+  }
+
+  join () {
+    const { playerNumber, playerName } = this.options;
+    this.send('join', { playerNumber, playerName });
+  }
+
+  notify (method, detail) {
+    this.middlewares.forEach(v => v[method](detail));
+  }
+
+  use (Middleware) {
+    this.middlewares.push(new Middleware(this));
+  }
+
+  send (eventName, data) {
+
+  }
+}
+
+module.exports = HeartsClientBase;
