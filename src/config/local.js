e187c5df51fa8289b4bfc18c739914eb509006ac
Wilson Su <wilson_su@trend.com.tw>
2018-09-03 12:00:08 +0800

    fix local config generator

--- src/config/local.js
+++ src/config/local.js
@@ -59,28 +59,28 @@ module.exports = {
   clients: [
     new HeartsClientLocal({
       playerNumber: 1,
-      playerName: ${options.name1},
+      playerName: '${options.name1}',
       bot: new ${options.bot1}(),
       middlewares: [HeartsClientMiddleware],
       logs: path.join(__dirname, '${options.logs}'),
     }),
     new HeartsClientLocal({
       playerNumber: 2,
-      playerName: ${options.name2},
+      playerName: '${options.name2}',
       bot: new ${options.bot2}(),
       middlewares: [HeartsClientMiddleware],
       logs: path.join(__dirname, '${options.logs}'),
     }),
     new HeartsClientLocal({
       playerNumber: 3,
-      playerName: ${options.name3},
+      playerName: '${options.name3}',
       bot: new ${options.bot3}(),
       middlewares: [HeartsClientMiddleware],
       logs: path.join(__dirname, '${options.logs}'),
     }),
     new HeartsClientLocal({
       playerNumber: 4,
-      playerName: ${options.name4},
+      playerName: '${options.name4}',
       bot: new ${options.bot4}(),
       middlewares: [HeartsClientMiddleware],
       logs: path.join(__dirname, '${options.logs}'),
@@ -89,6 +89,6 @@ module.exports = {
 };
 `;
 
-const output = 'verify.config.js';
+const output = 'local.config.js';
 
 module.exports = { schema, generate, output };
