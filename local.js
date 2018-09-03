773d058e8389205f6aac27c7c743d111fb7e3076
Wilson Su <wilson_su@trend.com.tw>
2018-09-03 15:46:07 +0800

    update local script for option.deals

--- local.js
+++ local.js
@@ -1,6 +1,10 @@
+const path = require('path');
+const util = require('./src/shared/util');
 const HeartsCore = require('./src/hearts/HeartsCore');
 const config = require('./local.config');
 
-const core = new HeartsCore();
+const detail = config.record ? util.file.read(path.resolve(__dirname, config.logs, config.record, 'detail.json')) : null;
+const options = { deals: detail ? detail.match.games[0].deals : null };
+const core = new HeartsCore(options);
 
 config.clients.forEach(client => core.add(client));
