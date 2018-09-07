3c7ac101ac86d3bcb04646134b6ac16aba5540ab
Wilson Su <wilson_su@trend.com.tw>
2018-09-07 11:40:34 +0800

    parse script code refactoring

--- parse.js
+++ parse.js
@@ -1,20 +1,28 @@
 const path = require('path');
 const grab = require('ps-grab');
 const util = require('./src/shared/util');
+const Logger = require('./src/shared/Logger');
 const HeartsClientMiddleware = require('./src/hearts/HeartsClientMiddleware');
 const HeartsClientBase = require('./src/hearts/HeartsClientBase');
 const HeartsBotBase = require('./src/hearts/HeartsBotBase');
+const parser = {
+  file: source => {
+    const filepath = path.resolve(__dirname, `${source}.json`);
+
+    if (!util.file.exist(filepath)) { return console.log(`File '${filepath}' not exists`); }
+
+    const log = util.file.read(filepath);
+    const bot = new HeartsBotBase();
+    const logs = path.join(__dirname, 'logs');
+    const logger = new Logger('warn');
+    const options = { bot, logs, logger };
+    const client = new HeartsClientBase(options);
+    const middleware = new HeartsClientMiddleware(client);
+
+    log.events.forEach(e => middleware.onMessage(e));
+  }
+};
 
 const source = grab('--source');
-const filepath = path.resolve(__dirname, `${source}.json`);
 
-if (!util.file.exist(filepath)) { return console.log(`File '${filepath}' not exists`); }
-
-const log = util.file.read(filepath);
-const bot = new HeartsBotBase();
-const logs = path.join(__dirname, 'logs');
-const options = { bot, logs };
-const client = new HeartsClientBase(options);
-const middleware = new HeartsClientMiddleware(client);
-
-log.events.forEach(e => middleware.onMessage(e));
+source && parser.file(source);
