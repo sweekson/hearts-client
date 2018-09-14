a852101e8ce0a88f77a3d38072ebcd150acb2812
Wilson Su <wilson_su@trend.com.tw>
2018-09-14 19:58:49 +0800

    try retreiving logger from config

--- local.js
+++ local.js
@@ -4,7 +4,8 @@ const HeartsCore = require('./src/hearts/HeartsCore');
 const config = require('./local.config');
 
 const detail = config.record ? util.file.read(path.resolve(__dirname, config.logs, config.record, 'detail.json')) : null;
-const options = { deals: detail ? detail.match.games[0].deals : null };
+const logger = config.logger;
+const options = { deals: detail ? detail.match.games[0].deals : null, logger };
 const core = new HeartsCore(options);
 
 config.clients.forEach(client => core.add(client));
