686ea8f92d6585defc71ab2234b12ca94cbcaa5a
Wilson <sweekson@gmail.com>
2018-09-16 23:00:38 +0800

    add reports config for local hearts

--- local.js
+++ local.js
@@ -5,7 +5,8 @@ const config = require('./local.config');
 
 const detail = config.record ? util.file.read(path.resolve(__dirname, config.logs, config.record, 'detail.json')) : null;
 const logger = config.logger;
-const options = { deals: detail ? detail.match.games[0].deals : null, logger };
+const reports = config.reports;
+const options = { deals: detail ? detail.match.games[0].deals : null, logger, reports };
 const core = new HeartsCore(options);
 
 config.clients.forEach(client => core.add(client));
