6dc403ae8957f824ff4d90b3e80111a65c072d63
Wilson <sweekson@gmail.com>
2018-09-21 02:49:10 +0800

    update HeartsAverageScoreHandler model

--- src/hearts/HeartsAverageScoreHandler.js
+++ src/hearts/HeartsAverageScoreHandler.js
@@ -1,5 +1,9 @@
 const HeartsLocalHandlerBase = require('./HeartsLocalHandlerBase');
 
+const ratings = {
+  0: 20, 1: 5, 2: -5, 3: -20
+};
+
 class HeartsAverageScoreHandler extends HeartsLocalHandlerBase {
   constructor () {
     super();
@@ -18,14 +22,20 @@ class HeartsAverageScoreHandler extends HeartsLocalHandlerBase {
       const scores = previous.scores || [];
       const total = (previous.total || 0) + score;
       scores.push(score);
-      this.players.set(number, Object.assign(previous, { number, scores, total }));
+      this.players.set(number, Object.assign(previous, { number, score, scores, total }));
+    });
+    [...this.players.values()].sort((a, b) => b.score - a.score).forEach((player, i) => {
+      this.players.set(player.number, Object.assign(player, {
+        rating: (player.rating || 1500) + ratings[i]
+      }));
     });
   }
 
   complete (core) {
     console.log('Average Game Score');
-    [...this.players.values()].forEach(player => {
-      console.log(`Player ${player.number}: ${player.total / this.games}`);
+    [...this.players.values()].forEach(({ number, total, rating }) => {
+      const average = (total / this.games).toFixed(2);
+      console.log(`Player ${number}: ${average}, Rating: ${rating}`);
     });
     console.log();
   }
