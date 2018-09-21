91dec5ac97013117c96d6a9ee87dd108328095c7
Austin Hung <austin_h_hung@trend.com.tw>
2018-09-21 20:34:32 +0800

    Refactoring ScoreLessBot modal

--- src/hearts/ScoreLessBot.js
+++ src/hearts/ScoreLessBot.js
@@ -1,27 +1,23 @@
-const HeartsBotBase = require('./HeartsBotBase');
 const { Cards, Card } = require('./HeartsDataModels');
 const HeartsRiskEvaluateBot = require('./HeartsRiskEvaluateBot');
+const { PowerRiskCard, PowerRiskCards } = HeartsRiskEvaluateBot;
 
-class ScoreLessBot extends HeartsBotBase {
-  constructor() {
-    super();
+class ScoreLessBot extends HeartsRiskEvaluateBot {
+  constructor(options) {
+    super(options);
     this.isFirst = false;
     this.valid = {};
-    this.dealPlayedCard = {};
     this.AS = new Card('AS');
     this.KS = new Card('KS');
     this.QS = new Card('QS');
-    this.REVABot = new HeartsRiskEvaluateBot();
-  }
-  pass(middleware) {
-    return this.REVABot.pass(middleware);
-  }
-
-  expose(middleware) {
-    return this.REVABot.shootTheMoon ? ['AH'] : [];
   }
 
   pick(middleware) {
+    const card = this.pickCard(middleware);
+    return card.value;
+  }
+
+  pickCard(middleware) {
     const round = middleware.round;
     const hand = middleware.hand;
     const deal = middleware.deal;
@@ -33,23 +29,21 @@ class ScoreLessBot extends HeartsBotBase {
     this.dealPlayedCards = deal.played;
     const shouldPickQueenSpade = followed.gt('QS').length && this.valid.contains('QS');
     const shouldPickTenClub = followed.gt('TC').length && this.valid.contains('TC');
-    const shootTheMoonNow = this.REVABot.shouldShootTheMoonNow(middleware);
+    const shootTheMoonNow = this.shootTheMoonNow = this.shouldShootTheMoonNow(middleware);
 
     if (handCards.length === 1 || this.valid.length === 1) {
       return this.valid.max;
     }
     //Shoot The Moon
-    if (this.REVABot.shootTheMoon || shootTheMoonNow) {
-      return this.REVABot.pick(middleware);
+    if (this.shootTheMoon || shootTheMoonNow) {
+      return this.findBestCard(middleware);
     }
     //Leader
     if (this.isFirst) {
-      this.detail.picked = this.confirmCards();
-      return this.REVABot.findBetterCard(middleware) || this.confirmCards() || this.valid.skip('QS').min || this.valid.min;
+      return this.findBetterCard(middleware) || this.confirmCards() || this.valid.skip('QS').min || this.valid.min;
     }
     //Void
     if (!hand.canFollowLead) {
-      this.detail.picked = this.confirmCards();
       return this.valid.find('QS') || this.valid.find('AS') || this.valid.find('KS') || this.valid.find('TC') || this.confirmCards();
     }
     //Follow
@@ -66,80 +60,69 @@ class ScoreLessBot extends HeartsBotBase {
       return this.valid.lt(followed.max).max || this.valid.max;
     }
     if (!round.isLast) {
-      return this.REVABot.findBetterCard(middleware) || this.valid.lt(followed.max).max || this.valid.skip('QS', 'TC').min || this.valid.min;
+      return this.findBetterCard(middleware) || this.valid.lt(followed.max).max || this.valid.skip('QS', 'TC').min || this.valid.min;
     }
   }
 
   confirmCards() {
+    const { valid, dealPlayedCards, isFirst, detail } = this;
     const candidateCards = this.getCandidateCards();
-    const pick = { card: null, score: 0, length: 0, lessSmallCardCount: 0 };
-    const select = (card, score, length, lessSmallCardCount) => {
-      pick.card = card;
-      pick.score = score;
-      pick.length = length;
-      pick.lessSmallCardCount = lessSmallCardCount;
-    };
-    this.detail.isFirst = this.isFirst;
-    this.detail.candidateCards = candidateCards;
-    this.detail.evaluated = [];
-    candidateCards.each(card => {
-      const number = card.number;
-      const length = this.valid.suit(card.suit).length;
-      const smallPlayedCount = this.dealPlayedCards.suit(card.suit).lt(card).length;
-      const largePlayedCount = this.dealPlayedCards.suit(card.suit).gt(card).length;
-      const score = this.getScore(number, length, smallPlayedCount, largePlayedCount);
-      const lessSmallCardCount = number - 2 - smallPlayedCount //2 is the small card number
-      this.detail.evaluated.push(card.value + `(${score})`);
-      if ((card.is(this.QS.value) || card.is(this.KS.value)) && length === 1) { return; } //** Never play only KS or QS
-      !pick.card && select(card, score, length, lessSmallCardCount);
-      score > pick.score && select(card, score, length, lessSmallCardCount);
-      score === pick.score && (this.isFirst ? lessSmallCardCount < pick.lessSmallCardCount : lessSmallCardCount > pick.lessSmallCardCount) && select(card, score, length, lessSmallCardCount);
-      score === pick.score && lessSmallCardCount === pick.lessSmallCardCount && length < pick.length && select(card, score, length, lessSmallCardCount);
-    });
-    return pick.card;
+    Object.assign(detail, { candidateCards });
+
+    const evaluated = this.getCardScores(valid, candidateCards, dealPlayedCards, isFirst);
+    Object.assign(detail, { candidateCards, evaluated });
+    if(!evaluated.length) { return; };
+
+    const highCards = new PowerRiskCards(evaluated.list.filter(c => c.power === evaluated.strongest.power));
+    Object.assign(detail, { candidateCards, evaluated, highCards});
+    if(highCards.length === 1) {
+      return detail.selected = highCards.first;
+    }
+
+    const riskCards = new PowerRiskCards(highCards.list.filter(c => c.risk === (isFirst ? highCards.safety.risk : highCards.risky.risk)));
+    Object.assign(detail, { candidateCards, evaluated, highCards, riskCards });
+    if(riskCards.length === 1) {
+      return detail.selected = riskCards.first;
+    }
+
+    return detail.selected = new Cards(riskCards.list.sort((a, b) => valid.suit(a.suit).length - valid.suit(b.suit).length)).first;
   }
 
   getCandidateCards() {
-    const minCards = [
-      this.valid.skip('QS').spades.min,
-      this.valid.hearts.min,
-      this.valid.diamonds.min,
-      this.valid.skip('TC').clubs.min,
-    ];
-    const maxCards = [
-      this.valid.skip('QS').spades.max,
-      this.valid.hearts.max,
-      this.valid.diamonds.max,
-      this.valid.clubs.max,
-    ];
-    return new Cards((this.isFirst ? minCards : maxCards).filter(v => !!v));
+    return new Cards((
+      this.isFirst ? [
+        this.valid.skip('QS').spades.min,
+        this.valid.hearts.min,
+        this.valid.diamonds.min,
+        this.valid.skip('TC').clubs.min,
+      ] : [
+        this.valid.skip('QS').spades.max,
+        this.valid.hearts.max,
+        this.valid.diamonds.max,
+        this.valid.clubs.max,
+      ])
+      .filter(v => !!v)
+    );
   }
 
-  getScore(number, length, smallPlayedCount, largePlayedCount) {
-    const a = number - 2 - smallPlayedCount;
-    const b = 14 - number - (length - 1) - largePlayedCount;
-    const c = length * length;
-    const coefficient = (b + a) / c;
-    const score = this.isFirst ? (b - a) + coefficient : (a - b) + coefficient;
-    return score;
-  }
-
-  onNewDeal(middleware) {
-    middleware.hand.detail = {};
-    this.REVABot.onNewDeal(middleware);
-  }
-
-  onPassCardsEnd(middleware) {
-    this.REVABot.onPassCardsEnd(middleware);
-  }
-
-  onNewRound(middleware) {
-    middleware.round.detail = {};
-    this.REVABot.onNewRound(middleware);
-  }
-
-  onRoundEnd(middleware) {
-    this.REVABot.onRoundEnd(middleware);
+  getCardScores(valid, selected, played, isFirst) {
+    return new PowerRiskCards(
+      selected.list.map(card => {
+        const all = Cards.instanciate(Cards.deck).suit(card.suit);
+        const alt = all.lt(card).length;
+        const agt = all.gt(card).length;
+        const plt = played.suit(card.suit).lt(card).length;
+        const pgt = played.suit(card.suit).gt(card).length;
+        const vlt = valid.suit(card.suit).lt(card).length;
+        const vgt = valid.suit(card.suit).gt(card).length;
+        const vsuit = valid.suit(card.suit).length;
+        const olt = alt - plt - vlt; // a
+        const ogt = agt - pgt - vgt; // b
+        const coefficient = (ogt + olt) / (vsuit * vsuit);
+        const score = isFirst ? (ogt - olt) + coefficient : (olt - ogt) + coefficient;
+        return new PowerRiskCard(card.value, olt, score);
+      })
+    );
   }
 }
 
