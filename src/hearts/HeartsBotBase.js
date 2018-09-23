19667903b74386796ed9de96cd7ac835813f2c67
Wilson <sweekson@gmail.com>
2018-09-23 23:32:58 +0800

    update HeartsBotBase model for strategy estimation

--- src/hearts/HeartsBotBase.js
+++ src/hearts/HeartsBotBase.js
@@ -1,10 +1,25 @@
 const Logger = require('../shared/Logger');
 
+/**
+ * - roles.shooter (Default: true) Try to shoot the moon if matching default rules
+ * - roles.guard (Default: true) Try to stop opponent shoot the moon in each deal
+ *
+ * - observed.radicals (Default: []) Oppnents who often try to shoot the moon
+ * - observed.terminators (Default: []) Oppnents who often stop others shoot the moon
+ *
+ * - hasRadical (Default: true)
+ *   Using STRICT rules for shooting the moon if at least one current oppnent is listed in observed.dedicals; otherwise, change strategy to aggressive which using LOOSE rules during a game
+ * - hasTerminator (Default: false)
+ *   Try to shoot the moon if there is not any oppnent is listed in observed.terminators; otherwise, change strategy to strictest which won't shoot the moon during a game
+ */
+
 class HeartsBotBase {
   constructor (options = {}) {
     this.options = options;
     this.roles = Object.assign({ shooter: true,  guard: true }, options.roles);
-    this.aggressors = Object.assign({ radicals: [], terminators: [] }, options.aggressors);
+    this.observed = Object.assign({ radicals: [], terminators: [] }, options.observed);
+    this.hasRadical = true;
+    this.hasTerminator = false;
     this.logger = options.logger || new Logger('info');
   }
 
@@ -18,6 +33,11 @@ class HeartsBotBase {
   }
 
   onNewGame (middleware) {
+    const { radicals, terminators } = this.observed;
+    const players = middleware.match.players;
+    const contains = targets => players.list.some(v => targets.indexOf(v.number) > -1);
+    this.hasRadical = radicals.length ? contains(radicals) : this.hasRadical;
+    this.hasTerminator = terminators.length ? contains(terminators) : this.hasTerminator;
   }
 
   onNewDeal (middleware) {
