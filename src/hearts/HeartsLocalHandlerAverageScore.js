dca548d37ee4b1ce61869b835c3f7956fbe2be58
Wilson <sweekson@gmail.com>
2018-09-24 11:37:49 +0800

    update HeartsLocalHandlerAverageScore model for Ranks Stats

--- src/hearts/HeartsLocalHandlerAverageScore.js
+++ src/hearts/HeartsLocalHandlerAverageScore.js
@@ -8,6 +8,7 @@ class HeartsLocalHandlerAverageScore extends HeartsLocalHandlerBase {
   constructor () {
     super();
     this.players = new Map();
+    this.ranks = new Map();
     this.games = 0;
   }
 
@@ -21,13 +22,20 @@ class HeartsLocalHandlerAverageScore extends HeartsLocalHandlerBase {
       const previous = this.players.get(number) || player;
       const scores = previous.scores || [];
       const total = (previous.total || 0) + score;
+      const ranks = previous.ranks || { rank1: 0, rank2: 0, rank3: 0, rank4: 0 };
       scores.push(score);
-      this.players.set(number, Object.assign(previous, { number, score, scores, total }));
+      this.players.set(number, Object.assign(previous, { number, score, scores, total, ranks }));
     });
     [...this.players.values()].sort((a, b) => b.score - a.score).forEach((player, i) => {
       this.players.set(player.number, Object.assign(player, {
         rating: (player.rating || 1500) + ratings[i]
       }));
+      Object.assign(player.ranks, {
+        rank1: player.ranks.rank1 + (i === 0 ? 1 : 0),
+        rank2: player.ranks.rank2 + (i === 1 ? 1 : 0),
+        rank3: player.ranks.rank3 + (i === 2 ? 1 : 0),
+        rank4: player.ranks.rank4 + (i === 3 ? 1 : 0),
+      });
     });
   }
 
@@ -36,6 +44,16 @@ class HeartsLocalHandlerAverageScore extends HeartsLocalHandlerBase {
     [...this.players.values()].forEach(({ number, total, rating }) => {
       const average = (total / this.games).toFixed(2);
       console.log(`Player ${number}: ${average}, Rating: ${rating}`);
+    });
+    console.log();
+
+    console.log('Rank Stats');
+    [...this.players.values()].forEach(({ number, ranks }) => {
+      const rank1 = `1 (${ranks.rank1})`;
+      const rank2 = `2 (${ranks.rank2})`;
+      const rank3 = `3 (${ranks.rank3})`;
+      const rank4 = `4 (${ranks.rank4})`;
+      console.log(`Player ${number}:`, rank1, rank2, rank3, rank4);
     });
     console.log();
   }
