df2433f1d04adf4c080c79cf2e5d62081ae50640
Wilson <sweekson@gmail.com>
2018-10-02 11:23:47 +0800

    fix constructor of HeartsCardPassBase model

--- src/hearts/HeartsCardPassBase.js
+++ src/hearts/HeartsCardPassBase.js
@@ -1,11 +1,10 @@
 
 class HeartsCardPassBase {
-  constructor({ match, game, deal, hand, round }) {
+  constructor({ match, game, deal, hand }) {
     this.match = match;
     this.players = this.match.players;
     this.game = game;
     this.deal = deal;
-    this.played = deal.played;
     this.hand = hand;
     this.cards = hand.cards;
     this.valid = hand.valid;
@@ -14,11 +13,6 @@ class HeartsCardPassBase {
     this.hearts = this.cards.hearts;
     this.diamonds = this.cards.diamonds;
     this.clubs = this.cards.clubs;
-    this.canFollowLead = hand.canFollowLead;
-    this.round = round;
-    this.lead = round.lead;
-    this.followed = round.followed;
-    this.detail = round.detail;
   }
   pass() {
     return this.valid.select(0, 3);
