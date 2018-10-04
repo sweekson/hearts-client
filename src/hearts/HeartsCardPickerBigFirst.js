2dd7016af098520a096f0f73d5a4b073680da64f
Wilson <sweekson@gmail.com>
2018-10-05 02:12:57 +0800

    fix pick of HeartsCardPickerBigFirst model

--- src/hearts/HeartsCardPickerBigFirst.js
+++ src/hearts/HeartsCardPickerBigFirst.js
@@ -2,17 +2,20 @@ const HeartsCardPickerSkeleton = require('./HeartsCardPickerSkeleton');
 
 /**
  * 1. This model might return `undefined`
- * 2. This model DO NOT handle situation which self CAN NOT follow lead
+ * 2. This model DO NOT handle situation which self CANNOT follow lead
  */
 class HeartsCardPickerBigFirst extends HeartsCardPickerSkeleton {
   pick () {
-    const { deal, hand, round } = this;
+    const { deal, valid, round, followed } = this;
     const played = deal.played;
-    const { spades, hearts, diamonds, clubs } = hand.valid;
+    const { spades, hearts, diamonds, clubs } = valid;
     const { isFirst, lead, hasPenaltyCard } = round;
     const hasQueenSpade = spades.contains('QS');
     const hasTenClub = clubs.contains('TC');
     let candidate;
+    if (!isFirst && followed.max.gt(valid.max)) {
+      return valid.max;
+    }
     if (!hasPenaltyCard && (isFirst || lead.isSpade) && played.spades.length <= 2) {
       candidate = hasQueenSpade ? spades.find('AS') || spades.find('KS') || spades.lt('QS').max : spades.lt('QS').max;
     }
