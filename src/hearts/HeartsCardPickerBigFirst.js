969ef46ce513f822389536bd5f86284f4ac659c7
Wilson <sweekson@gmail.com>
2018-10-09 14:53:23 +0800

    improve HeartsCardPickerBigFirst model

--- src/hearts/HeartsCardPickerBigFirst.js
+++ src/hearts/HeartsCardPickerBigFirst.js
@@ -1,5 +1,5 @@
 const HeartsCardPickerSkeleton = require('./HeartsCardPickerSkeleton');
-const { PowerCards  } = require('./HeartsDataModels');
+const { Cards, PowerCards  } = require('./HeartsDataModels');
 
 /**
  * WARNING
@@ -16,21 +16,33 @@ class HeartsCardPickerBigFirst extends HeartsCardPickerSkeleton {
     const hasTenClub = clubs.contains('TC') || hand.gained.contains('TC');
     const hasFewPlayed = suit => played[suit].length <= 2;
     const hasBig = suit => eva1[suit].filter(v => v.power > -3).length > 0;
-    const isShort = suit => valid[suit].length <= 5;
-    let candidate;
+    const isShort = suit => valid[suit].length - 2 <= (13 - played[suit].length + round.played[suit].length) * .25;
+    const candidates = new Cards();
     if (!isFirst && followed.max.gt(valid.max)) {
       return valid.max;
     }
-    if (!hasPenaltyCard && (isFirst || lead.isSpade) && isShort('spades') && hasBig('spades') && hasFewPlayed('spades')) {
-      candidate = hasQueenSpade ? spades.find('AS') || spades.find('KS') || spades.lt('QS').max : spades.lt('QS').max;
+    if (isFirst && isShort('spades') && hasBig('spades') && hasFewPlayed('spades')) {
+      candidates.push(hasQueenSpade ? spades.find('AS') || spades.find('KS') || spades.lt('QS').max : spades.lt('QS').max);
     }
-    if (!hasPenaltyCard && !candidate && (isFirst || lead.isClub) && isShort('clubs') && hasBig('clubs') && hasFewPlayed('clubs')) {
-      candidate = hasTenClub ? clubs.gt('TC').max || clubs.lt('TC').max : clubs.lt('TC').max;
+    if (isFirst && isShort('clubs') && hasBig('clubs') && hasFewPlayed('clubs')) {
+      candidates.push(hasTenClub ? clubs.gt('TC').max || clubs.lt('TC').max : clubs.lt('TC').max);
     }
-    if (!hasPenaltyCard && !candidate && (isFirst || lead.isDiamond) && isShort('diamonds') && hasBig('diamonds') && hasFewPlayed('diamonds')) {
-      candidate = diamonds.max;
+    if (isFirst && isShort('diamonds') && hasBig('diamonds') && hasFewPlayed('diamonds')) {
+      candidates.push(clubs.min);
     }
-    return candidate;
+    if (isFirst) {
+      return candidates.length ? candidates.first : undefined;
+    }
+    if (lead.isSpade && !hasPenaltyCard && isShort('spades')) {
+      return hasQueenSpade ? spades.find('AS') || spades.find('KS') || spades.lt('QS').max : spades.lt('QS').max;
+    }
+    if (lead.isClub && !hasPenaltyCard && isShort('clubs')) {
+      return hasTenClub ? clubs.gt('TC').max || clubs.lt('TC').max : clubs.lt('TC').max;
+    }
+    if (lead.isDiamond && !hasPenaltyCard && isShort('diamonds')) {
+      return diamonds.max;
+    }
+    return undefined;
   }
 }
 
