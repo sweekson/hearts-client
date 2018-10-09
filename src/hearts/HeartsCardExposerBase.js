949fa4a60664319f13e771449628cf06eee79c10
Wilson <sweekson@gmail.com>
2018-10-10 00:57:08 +0800

    revise HeartsCardExposerBase model

--- src/hearts/HeartsCardExposerBase.js
+++ src/hearts/HeartsCardExposerBase.js
@@ -24,13 +24,17 @@ class HeartsCardExposerBase {
     const hl = h.length;
     const dl = d.length;
     const cl = c.length;
+    const hasOneHalfSuit = sl >= 6 || hl >= 6 || dl >= 6 || cl >= 6;
+    const hasOneLongSuit = sl >= 8 || hl >= 8 || dl >= 8 || cl >= 8;
     const ss = s.strength;
     const hs = h.strength;
     const ds = d.strength;
     const cs = c.strength;
     const ts = ss + hs + ds + cs;
-    if ((sl + ss) >= 27 && (hl + hs) >= 22 && ts >= 52) { return ['AH']; }
-    if (hs <= 9 && hl >= 5) { return ['AH']; }
+    if ((sl + ss) >= 8 && (hl + hs) >= 13 && ts >= 30) { return ['AH']; }
+    if (ss >= 15 && hs >= 8 && hasOneHalfSuit) { return ['AH']; }
+    if (ss >= 4 && sl >= 3 && hasOneLongSuit) { return ['AH']; }
+    if (hs <= 7 && hl >= 5) { return ['AH']; }
     return [];
   }
 }
